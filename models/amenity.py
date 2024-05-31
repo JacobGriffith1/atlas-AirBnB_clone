298a300d259420d677ab570925b455340fc145c2
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-05-31 18:18:50 -0500

    amenity.py: class: 'Amenity' added

---
+++ models/amenity.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Module contains class: 'Amenity'"""
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """Defines amenities for user selection"""
+    name = ""
