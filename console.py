aef5cb90ebc47368b7c74c4c385cdd9aa155b767
moshjerrick <155486477+moshjerrick@users.noreply.github.com>
2024-05-31 12:10:47 -0500

    I was writing and pushing console code to the wrong file. now its in the right file

---
+++ console.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+console module
+"""
+
+import cmd
+
+class HBNBCommand(cmd.Cmd):
+    """HBNB Class"""
+    prompt = '(hbnb) '
+
+    def do_quit(self, arg):
+        """ Define quit """
+        return True
+    
+    def do_EOF(self, arg):
+        """ Define end of function """
+        return True
