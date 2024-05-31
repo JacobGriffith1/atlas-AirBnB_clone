f78a605096ecfeb89cee798a92f88091cab1edaa
moshjerrick <155486477+moshjerrick@users.noreply.github.com>
2024-05-31 12:54:40 -0500

    do_create, checks if name is missing, or if it exists then creates an instance and saves it

--- console.py
+++ console.py
@@ -4,6 +4,14 @@ console module
 """
 
 import cmd
+import shlex
+from models.base_model import BaseModel
+
+class_registry = {
+    'BaseModel' : BaseModel,
+#add other model classes here, user, place etc
+
+}
 
 class HBNBCommand(cmd.Cmd):
     """HBNB Class"""
@@ -22,6 +30,23 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         """Do nothing on empty input line."""
         pass
+
+    def do_create(self, arg):
+        """ Creates new instance of BaseModel and saves it to JSON file """
+        args = shlex.split(arg)
+        if len(args) == 0:
+            print("** class name missing **")
+            return
+
+        class_name = args[0]
+        if class_name not in class_registry:
+            print("** class doesn't exist **")
+            return
+
+        # Creates an instance of a class
+        new_instance = class_registry[class_name]()
+        new_instance.save()
+        print(new_instance.id)
 
 
 if __name__ == '__main__':
