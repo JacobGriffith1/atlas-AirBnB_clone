2b9a917b9c3ba23d84bb08c65a1d4e7408cab0a0
moshjerrick <155486477+moshjerrick@users.noreply.github.com>
2024-05-31 15:02:35 -0500

    last tinker for me for the day

--- console.py
+++ console.py
@@ -6,7 +6,7 @@ console module
 import cmd
 import shlex
 from models.base_model import BaseModel
-from models import storage 
+from models import storage
 
 class_registry = {
     'BaseModel': BaseModel,
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """ Module contains class: FileStorage """
 import json
-
+import os
 
 class FileStorage:
     """ Serializes and deserializes JSON objects """
-    __file_path = file.json #string - path to the JSON file
+    __file_path = "file.json" #string - path to the JSON file
     __objects = {} #dictionary - empty but will store all objects by <class name>.id
 
     def all(self):
