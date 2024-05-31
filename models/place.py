d387c8d76ae122287ee659c022951d6c94d382c0
moshjerrick <155486477+moshjerrick@users.noreply.github.com>
2024-05-31 12:09:52 -0500

    pushing 6

--- models/place.py
+++ models/place.py
@@ -8,11 +8,11 @@ import cmd
 class HBNBCommand(cmd.Cmd):
     """HBNB Class"""
     prompt = '(hbnb) '
-    
-    def do_quit(self, argument):
+
+    def do_quit(self, arg):
         """ Define quit """
         return True
     
-    def do_EOF(self, arguemtn):
+    def do_EOF(self, arg):
         """ Define end of function """
         return True
