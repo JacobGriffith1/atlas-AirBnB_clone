fa4313512327ec6a9aee0755d378fb8a5d8c640e
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-05-31 19:29:07 -0500

    file_storage.py: Moved imports for reload into its def. PEP8 might not like it, but I'm doing this to see if it fixes the checker with the storage import for other files

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -2,13 +2,6 @@
 """ Module contains class: FileStorage """
 import json
 import os
-from models.base_model import BaseModel
-from models.user import User
-from models.place import Place
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.review import Review
 
 class FileStorage:
     """ Serializes and deserializes JSON objects """
@@ -43,6 +36,14 @@ class FileStorage:
         (only if the JSON file (__file_path) exists;
         otherwise, do nothing.)
         """
+        from models.base_model import BaseModel
+        from models.user import User
+        from models.place import Place
+        from models.state import State
+        from models.city import City
+        from models.amenity import Amenity
+        from models.review import Review
+
         dictionary = {'BaseModel': BaseModel, 'User': User, 'Place': Place,
                     'State': State, 'City': City,
                     'Amenity': Amenity, 'Review': Review}
