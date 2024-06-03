9804b56d1f6632dfbf9b31cbaec04c5f77197a1b
BPChance <bradenpchance6@gmail.com>
2024-06-03 13:44:19 -0500

    created folder test_engine in test_models

---
+++ tests/test_models/test_engine/test_file_storage.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+""" unittest for file_storage.py """
+import unittest
+import os
+import json
+from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
+
+
+class Test_File_Storage(unittest.TestCase):
+    """ test cases for FileStorage class """
+
+    def test_save(self):
+        """ test that save method serializes '__objects' to the JSON file """
+        
+        # call save method
+        self.storage.save()
+
+        # check that the file was created
+        self.assertTrue(os.path.exists(self.file_path))
+
+        # check that file contains correct data
+        with open(self.file_path, 'r') as f:
+            data = json.load(f)
+
+        key = "{}.{}".format(self.test_obj.__class__.__name__, "1234")
+        # check that key is in JSON file
+        self.assertIn(key, data)
+        # verify that data in the file matches test objects attributes
+        self.assertEqual(data[key]['id'], "1234")
+        self.assertEqual(data[key]['__class__'], self.test_obj__class__.__name__)
+
+if __name__ == '__main__':
+    unittest.main()
