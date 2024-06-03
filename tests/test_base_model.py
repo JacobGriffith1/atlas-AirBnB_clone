15a5cec507387a75b67462c6f5eab5df4d54f38b
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-06-03 17:50:26 -0500

    test_base_model.py: Pycodestyle fixing

--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -4,6 +4,7 @@ import unittest
 from datetime import datetime
 from models.base_model import BaseModel
 
+
 class TestBaseModel(unittest.TestCase):
     """Test cases for base model"""
 
@@ -29,8 +30,11 @@ class TestBaseModel(unittest.TestCase):
         model_dict = model.to_dict()
         # check that id, created_at, and updated_at are set and are strings
         self.assertEqual(model_dict['id'], model.id)
-        self.assertEqual(model_dict['created_at'], model.updated_at.isoformat())
-        self.assertEqual(model_dict['updated_at'], model.updated_at.isoformat())
+        self.assertEqual(model_dict['created_at'],
+                         model.updated_at.isoformat())
+        self.assertEqual(model_dict['updated_at'],
+                         model.updated_at.isoformat())
+
 
 if __name__ == '__main__':
     unittest.main()
