ec3c53bb8f2989540681621d47b6039b0f684a42
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-06-03 23:17:18 -0500

    test_base_model.py: test_save_json added

--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -18,19 +18,27 @@ class TestBaseModel(unittest.TestCase):
         self.assertIsInstance(model, BaseModel)
         # check that id is string
         self.assertIsInstance(model.id, str)
-    
+
     def test_save_method(self):
         """test the save method"""
         model = BaseModel()
         Update = model.updated_at
         model.save()
         self.assertNotEqual(Update, model.updated_at)
-    
+
     def test_save_arg(self):
         """test save with an argument passed"""
         model = BaseModel()
         with self.assertRaises(TypeError):
             model.save(None)
+
+    def test_save_json(self):
+        """test save and read it"""
+        model = BaseModel()
+        model.save()
+        modelid = "BaseModel." + model.id
+        with open("file.json", "r") as f:
+            self.assertIn(modelid, f.read())
 
     def test_to_dict_method(self):
         """test the to_dict method"""
